13e73fe95136330e3176c055ae5d7385d963daa6
Augusto Sandim <augustosandim@gmail.com>
2021-04-01 15:54:05 -0400

    Add fails test

---
+++ src/pages/summary/test/SummaryForm.test.jsx
@@ -0,0 +1,30 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import SummaryForm from '../SummaryForm';
+
+test('Initial conditions', () => {
+  render(<SummaryForm />);
+
+  const checkbox = screen.getByRole('checkbox', {
+    name: /terms and conditions/i,
+  });
+
+  expect(checkbox).not.toBeChecked();
+
+  const confirmButton = screen.getByRole('buton', { name: /confirm order/i });
+  expect(confirmButton).toBeDisabled();
+});
+
+test('Checkbox enables button on first click and disables on second click', () => {
+  render(<SummaryForm />);
+
+  const checkbox = screen.getByRole('checkbox', {
+    name: /terms and conditions/i,
+  });
+  const confirmButton = screen.getByRole('buton', { name: /confirm order/i });
+
+  fireEvent.click(checkbox);
+  expect(confirmButton).toBeEnabled();
+
+  fireEvent.click(checkbox);
+  expect(confirmButton).toBeDisabled();
+});
